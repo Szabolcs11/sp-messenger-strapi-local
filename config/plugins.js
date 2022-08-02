f4e7a3ca688f966e2213b72d5287cc7ce15613d0
Szabolcs11 <67874109+Szabolcs11@users.noreply.github.com>
2022-08-02 11:50:48 +0200

    Add files via upload

---
+++ config/plugins.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//     "users-permissions": {
+//         config: {
+//           jwtSecret: env(
+//             "JWT_SECRET",
+//             "FLhrePTwXom6mVKIII+KX+IjYTU49xNwnznhT5HFPFg="
+//           ),
+//         },
+//       },
+    
+// }
